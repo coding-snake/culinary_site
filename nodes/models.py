b60ae32b2898d8d55b189df09fa6084b4e90da3b
coding-snake <tabiszewski.damian.008@gmail.com>
2024-10-07 02:56:33 +0200

    working on nodes: edge type choices

--- nodes/models.py
+++ nodes/models.py
@@ -10,8 +10,16 @@ class Node(models.Model):
         return self.title
     
 class Edge(models.Model):
+    EDGE_TYPE_CHOICES = (
+        (1, 'next'),
+        (2, 'required-prev')
+    )
+
     from_node = models.ForeignKey(Node, related_name="edges_from", on_delete=models.CASCADE)
     to_node = models.ForeignKey(Node, related_name="edges_to", on_delete=models.CASCADE)
-
+    edge_type = models.IntegerField(choices=EDGE_TYPE_CHOICES, default=1)
+    
     def __str__(self):
-        return f"{self.from_node} → {self.to_node}"
+        edge_type_in_str = dict(self.EDGE_TYPE_CHOICES).get(self.edge_type, "unknown")
+        return f"{self.from_node} → {self.to_node} ({edge_type_in_str})"
+    
